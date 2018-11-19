68313095e07660dbdf55c9726bf23749265865b1
downerj <downer.james.256k@gmail.com>
2018-11-19 00:58:36 -0700

    Week09: Add comments

--- public/index.js
+++ public/index.js
@@ -1,17 +1,26 @@
+/**
+ * GET ELEMENT BY ID
+ * Alias for document.getElementById.
+ */
 function byId(id) {
     return document.getElementById(id);
 }
 
+/**
+ * HTML ELEMENTS
+ */
 let divAjax;
 let divAjaxResponse;
 let selOutput;
 let frmMain;
 let btRequest;
 
-function ajax() {
-
-}
-
+/**
+ * VALIDATE INPUTS
+ * This function checks to see if all form inputs are valid.
+ * This is used for the button disabled and submit logic.
+ * @returns Whether or not the form is valid
+ */
 function validateInputs() {
     const validTypes = [
         'letter-stamped',
@@ -36,6 +45,11 @@ function validateInputs() {
     return validType && validWeight && validOutput;
 }
 
+/**
+ * ON OUTPUT SELECTOR CHANGE
+ * This function is triggered when the "Output Type" dropdown value is changed.
+ * It determines whether or not to hide and clear the AJAX/AJAX Reponse divs.
+ */
 function onOutputSelectorChange() {
     divAjaxResponse.innerHTML = '';
 
@@ -46,6 +60,11 @@ function onOutputSelectorChange() {
     }
 }
 
+/**
+ * ON WINDOW LOAD
+ * This function initializes the local elem variables and performs initial
+ * trigger calls.
+ */
 function onWindowLoad() {
     divAjax = byId('div-ajax');
     divAjaxResponse = byId('div-ajax-response');
@@ -58,10 +77,22 @@ function onWindowLoad() {
     onFormChange();
 }
 
+/**
+ * ON FORM CHANGE
+ * This function is triggered by the form's inputs changing.
+ * It determines whether or not the "Calculate" button is disabled.
+ */
 function onFormChange() {
     btRequest.disabled = !validateInputs();
 }
 
+/**
+ * ON REQUEST CLICK
+ * This function is triggered by the "Calculate" button clicking.
+ * If the form is valid, then this function either performs a basic HTTP form submission
+ * or an AJAX request.
+ * If doing an AJAX request, then this function updates the "AJAX Response" div.
+ */
 function onRequestClick() {
     switch (selOutput.value) {
         case 'html':
@@ -91,4 +122,5 @@ function onRequestClick() {
     }
 }
 
+// register the window load
 window.addEventListener('load', onWindowLoad);
